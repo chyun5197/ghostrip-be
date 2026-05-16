c07cd3face1bd73478689ba96909cfdebb1adf31
chyun5197 <chyun5197@gmail.com>
2026-05-16 14:23:59 +0900

    Initial commit

---
+++ src/test/java/com/example/neohack/NeoHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neohack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoHackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
