594ef5792f41d09257deab3a21fe746416dba1f9
chyun5197 <chyun5197@gmail.com>
2026-05-17 01:00:06 +0900

    feat:

--- src/main/java/com/example/neohack/domain/spot/service/SpotService.java
+++ src/main/java/com/example/neohack/domain/spot/service/SpotService.java
@@ -1,6 +1,7 @@
 package com.example.neohack.domain.spot.service;
 
 import com.example.neohack.domain.spot.dto.request.SpotImageRequest;
+import com.example.neohack.domain.spot.dto.response.SpotDetailResponse;
 import com.example.neohack.domain.spot.dto.response.SpotImageResponse;
 import com.example.neohack.domain.spot.entity.Spot;
 import com.example.neohack.domain.spot.entity.SpotImage;
@@ -9,6 +10,7 @@ import com.example.neohack.domain.spot.repository.SpotRepository;
 import com.example.neohack.global.dto.response.exception.CustomException;
 import com.example.neohack.global.dto.response.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +22,14 @@ public class SpotService {
 
     private final SpotRepository spotRepository;
     private final SpotImageRepository spotImageRepository;
+
+    @Transactional(readOnly = true)
+    public List<SpotDetailResponse> getBestSpots() {
+        return spotRepository.findAllByOrderByViewCountDesc(PageRequest.of(0, 10))
+                .stream()
+                .map(SpotDetailResponse::from)
+                .toList();
+    }
 
     @Transactional(readOnly = true)
     public Spot getSpotDetail(Long spotId) {
